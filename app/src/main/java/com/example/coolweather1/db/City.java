48d5323e1d7d3847e98f55c0cfb1562f8e90063e
cutelixin <792075865@qq.com>
2018-06-19 22:58:29 +0800

    JI

---
+++ app/src/main/java/com/example/coolweather1/db/City.java
@@ -0,0 +1,39 @@
+package com.example.coolweather1.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id;
+    private String cityName;
+    private int cityCode;
+    private int provinceId;
+
+    private  int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getCityName(){
+        return cityName;
+    }
+
+    public void setCityName(String cityName){
+        this.cityName=cityName;
+    }
+    public int getCityCode(){
+        return cityCode;
+    }
+    public void setCityCode(int cityCode)
+    {
+        this.cityCode=cityCode;
+    }
+    public int getProvinceId(){
+        return provinceId;
+    }
+    public void setProvinceId(int provinceId){
+        this.provinceId=provinceId;
+    }
+
+
+}
